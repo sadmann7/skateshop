69cc4c02e5eaaba887bbeb5e9a3f6c81872fe749
sadmann7 <sadmann898@gmail.com>
2023-05-31 17:58:44 +0600

    remove env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,5 @@ import type { Config } from "drizzle-kit"
 
 export default {
   schema: "./src/db/schema.ts",
-  connectionString:
-    "mysql://3wbp4xqs1u46zvzytu87:pscale_pw_lUYuISXS7kqmigpmfW6bSMhnxb5lL8iNwcjXt4RN2Cz@aws.connect.psdb.cloud/skateshop?sslaccept=strict",
+  connectionString: process.env.DATABASE_URL,
 } satisfies Config
