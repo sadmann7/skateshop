5beeadfb837dfbd7e78ffa681ffb97e736c72a09
sadmann7 <sadmann898@gmail.com>
2023-06-01 19:00:54 +0600

    migration

--- drizzle/0000_luxuriant_random.sql
+++ drizzle/0000_luxuriant_random.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `products` (
-	`id` varchar(191) PRIMARY KEY NOT NULL,
+	`id` serial AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`name` varchar(191) NOT NULL,
 	`description` text,
 	`images` json,
@@ -8,18 +8,11 @@ CREATE TABLE `products` (
 	`quantity` int NOT NULL DEFAULT 1,
 	`inventory` int NOT NULL DEFAULT 1,
 	`rating` int NOT NULL DEFAULT 0,
-	`createdAt` datetime(3) NOT NULL DEFAULT (CURRENT_TIMESTAMP(3)),
-	`updatedAt` datetime(3) NOT NULL,
-	`storeId` varchar(191) NOT NULL);
+	`storeId` int NOT NULL);
 
 CREATE TABLE `stores` (
-	`id` varchar(191) PRIMARY KEY NOT NULL,
+	`id` serial AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	`userId` varchar(191) NOT NULL,
 	`name` varchar(191) NOT NULL,
 	`description` text,
-	`createdAt` datetime(3) NOT NULL DEFAULT (CURRENT_TIMESTAMP(3)),
-	`updatedAt` datetime(3) NOT NULL);
-
-DROP TABLE `Product`;
-DROP TABLE `ProductImage`;
-DROP TABLE `Store`;
+	`slug` text);
