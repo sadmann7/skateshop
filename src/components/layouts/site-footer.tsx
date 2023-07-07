f0deb57823b1f97c6c0663fbee44fb4ad708bcbb
sadmann7 <sadmann898@gmail.com>
2023-07-07 20:00:52 +0600

    move footerNav to site config

--- src/components/layouts/site-footer.tsx
+++ src/components/layouts/site-footer.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import { type FooterItem } from "@/types"
 
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
@@ -8,105 +7,6 @@ import { SubscribeToNewsletterForm } from "@/components/forms/subscribe-to-newsl
 import { Icons } from "@/components/icons"
 import { ThemeToggle } from "@/components/layouts/theme-toggle"
 import { Shell } from "@/components/shell"
-
-const footerItems: FooterItem[] = [
-  {
-    title: "Credits",
-    items: [
-      {
-        title: "OneStopShop",
-        href: "https://onestopshop.jackblatch.com/",
-        external: true,
-      },
-      {
-        title: "Acme Corp",
-        href: "https://acme-corp.jumr.dev/",
-        external: true,
-      },
-      {
-        title: "shadcn/ui",
-        href: "https://ui.shadcn.com/",
-        external: true,
-      },
-      {
-        title: "Taxonomy",
-        href: "https://tx.shadcn.com/",
-        external: true,
-      },
-    ],
-  },
-  {
-    title: "Help",
-    items: [
-      {
-        title: "About",
-        href: "/about",
-      },
-      {
-        title: "Contact",
-        href: "/contact",
-      },
-      {
-        title: "Terms",
-        href: "/terms",
-      },
-      {
-        title: "Privacy",
-        href: "/privacy",
-      },
-    ],
-  },
-  {
-    title: "Social",
-    items: [
-      {
-        title: "Twitter",
-        href: siteConfig.links.twitter,
-        external: true,
-      },
-      {
-        title: "GitHub",
-        href: siteConfig.links.githubAccount,
-        external: true,
-      },
-      {
-        title: "Discord",
-        href: siteConfig.links.discord,
-        external: true,
-      },
-      {
-        title: "cal.com",
-        href: siteConfig.links.calDotCom,
-        external: true,
-      },
-    ],
-  },
-  {
-    title: "Lofi",
-    items: [
-      {
-        title: "beats to study to",
-        href: "https://www.youtube.com/watch?v=jfKfPfyJRdk",
-        external: true,
-      },
-      {
-        title: "beats to chill to",
-        href: "https://www.youtube.com/watch?v=rUxyKA_-grg",
-        external: true,
-      },
-      {
-        title: "a fresh start",
-        href: "https://www.youtube.com/watch?v=rwionZbOryo",
-        external: true,
-      },
-      {
-        title: "coffee to go",
-        href: "https://www.youtube.com/watch?v=2gliGzb2_1I",
-        external: true,
-      },
-    ],
-  },
-]
 
 export function SiteFooter() {
   return (
@@ -135,7 +35,7 @@ export function SiteFooter() {
             aria-labelledby="footer-links-heading"
             className="grid flex-1 grid-cols-2 gap-10 sm:grid-cols-4"
           >
-            {footerItems.map((item) => (
+            {siteConfig.footerNav.map((item) => (
               <div key={item.title} className="space-y-3">
                 <h4 className="text-base font-medium">{item.title}</h4>
                 <ul className="space-y-3">
@@ -159,7 +59,7 @@ export function SiteFooter() {
           <section
             id="newsletter"
             aria-labelledby="newsletter-heading"
-            className="space-y-2.5"
+            className="space-y-3"
           >
             <h4 className="text-base font-medium">
               Subscribe to our newsletter
@@ -170,16 +70,16 @@ export function SiteFooter() {
         <section
           id="footer-bottom"
           aria-labelledby="footer-bottom-heading"
-          className="flex flex-col items-center justify-between space-y-1 md:flex-row"
+          className="flex items-center space-x-4"
         >
-          <div className="text-center text-sm leading-loose text-muted-foreground">
+          <div className="flex-1 text-left text-sm leading-loose text-muted-foreground">
             Built by{" "}
             <a
               aria-label="Kickflip tutorial on YouTube"
               href="https://twitter.com/sadmann17"
               target="_blank"
               rel="noreferrer"
-              className="font-semibold transition-colors hover:text-slate-950 dark:hover:text-slate-200"
+              className="font-semibold transition-colors hover:text-foreground"
             >
               Sadman
             </a>
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,9 +1,17 @@
-import type { MainNavItem } from "@/types"
+import type { FooterItem, MainNavItem } from "@/types"
 
 import { productCategories } from "@/config/products"
 import { slugify } from "@/lib/utils"
 
 export type SiteConfig = typeof siteConfig
+
+const links = {
+  twitter: "https://twitter.com/sadmann17",
+  github: "https://github.com/sadmann7/skateshop",
+  githubAccount: "https://github.com/sadmann7",
+  discord: "https://discord.com/users/sadmann7",
+  calDotCom: "https://cal.com/sadmann7",
+}
 
 export const siteConfig = {
   name: "Skateshop13",
@@ -53,11 +61,107 @@ export const siteConfig = {
       ],
     })),
   ] satisfies MainNavItem[],
-  links: {
-    twitter: "https://twitter.com/sadmann17",
-    github: "https://github.com/sadmann7/skateshop",
-    githubAccount: "https://github.com/sadmann7",
-    discord: "https://discord.com/users/sadmann7",
-    calDotCom: "https://cal.com/sadmann7",
-  },
+  links,
+  footerNav: [
+    {
+      title: "Credits",
+      items: [
+        {
+          title: "OneStopShop",
+          href: "https://onestopshop.jackblatch.com/",
+          external: true,
+        },
+        {
+          title: "Acme Corp",
+          href: "https://acme-corp.jumr.dev/",
+          external: true,
+        },
+        {
+          title: "shadcn/ui",
+          href: "https://ui.shadcn.com/",
+          external: true,
+        },
+        {
+          title: "Taxonomy",
+          href: "https://tx.shadcn.com/",
+          external: true,
+        },
+      ],
+    },
+    {
+      title: "Help",
+      items: [
+        {
+          title: "About",
+          href: "/about",
+          external: false,
+        },
+        {
+          title: "Contact",
+          href: "/contact",
+          external: false,
+        },
+        {
+          title: "Terms",
+          href: "/terms",
+          external: false,
+        },
+        {
+          title: "Privacy",
+          href: "/privacy",
+          external: false,
+        },
+      ],
+    },
+    {
+      title: "Social",
+      items: [
+        {
+          title: "Twitter",
+          href: links.twitter,
+          external: true,
+        },
+        {
+          title: "GitHub",
+          href: links.githubAccount,
+          external: true,
+        },
+        {
+          title: "Discord",
+          href: links.discord,
+          external: true,
+        },
+        {
+          title: "cal.com",
+          href: links.calDotCom,
+          external: true,
+        },
+      ],
+    },
+    {
+      title: "Decompress",
+      items: [
+        {
+          title: "beats to study to",
+          href: "https://www.youtube.com/watch?v=jfKfPfyJRdk",
+          external: true,
+        },
+        {
+          title: "beats to chill to",
+          href: "https://www.youtube.com/watch?v=rUxyKA_-grg",
+          external: true,
+        },
+        {
+          title: "a fresh start",
+          href: "https://www.youtube.com/watch?v=rwionZbOryo",
+          external: true,
+        },
+        {
+          title: "coffee to go",
+          href: "https://www.youtube.com/watch?v=2gliGzb2_1I",
+          external: true,
+        },
+      ],
+    },
+  ] satisfies FooterItem[],
 }
