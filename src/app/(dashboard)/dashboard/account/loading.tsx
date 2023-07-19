774ccb5471dcb4872e544ef4c243ddd00b774e40
10Sadique <jafarsjahan@gmail.com>
2023-07-19 17:36:49 +0600

    title fixed

--- src/app/(dashboard)/dashboard/account/loading.tsx
+++ src/app/(dashboard)/dashboard/account/loading.tsx
@@ -5,8 +5,8 @@ import { Shell } from "@/components/shells/shell"
 export default function AccountLoading() {
   return (
     <Shell variant="sidebar">
-      <Header title="Stores" description="Manage your stores." size="sm" />
-      <div className="grid gap-10 rounded-lg border p-4">
+      <Header title="Account" description="Manage your account settings" size="sm" />
+      <div className="grid gap-10 p-4 border rounded-lg">
         <div className="space-y-2">
           <Skeleton className="h-5 w-20" />
           <Skeleton className="h-4 w-72" />
