c7268295308621289e7838122fa26dfb88712389
sadmann7 <sadmann898@gmail.com>
2023-11-18 15:43:44 +0600

    update packages

---
+++ src/app/(rad)/ai/page.tsx
@@ -0,0 +1,9 @@
+import { Shell } from "@/components/shells/shell"
+
+export default function Ai() {
+  return (
+    <Shell>
+      <div>Ai page</div>
+    </Shell>
+  )
+}
---
+++ src/app/(rad)/layout.tsx
@@ -0,0 +1,3 @@
+export default function RadLayout({ children }: React.PropsWithChildren) {
+  return <>{children}</>
+}
