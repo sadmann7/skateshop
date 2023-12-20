079ff3fd570405082de2aff053d719d83bb8b009
JDLanctot <jordan.lanctot@torontomu.ca>
2023-12-20 15:35:10 -0500

    Adding a layout.tsx to the content folder to have navbar for this grouping of directories

---
+++ src/app/(lobby)/(content)/layout.tsx
@@ -0,0 +1,23 @@
+import { currentUser } from "@clerk/nextjs"
+
+import { SiteFooter } from "@/components/layouts/site-footer"
+import { SiteHeader } from "@/components/layouts/site-header"
+
+interface MainLayoutProps
+  extends React.PropsWithChildren<{
+    modal: React.ReactNode
+  }> {}
+
+export default async function MainLayout({ children }: MainLayoutProps) {
+  const user = await currentUser()
+
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      <SiteHeader user={user} />
+      <main className="flex-1">
+        {children}
+      </main>
+      <SiteFooter />
+    </div>
+  )
+}
