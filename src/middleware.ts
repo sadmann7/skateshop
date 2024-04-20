5edfbb18a4daa22db430f2d7f5b2cadddd2dc6c9
sadmann7 <sadmann898@gmail.com>
2024-04-20 19:22:26 +0600

    refactor: refactor middleware to use new clerkMiddleware API

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,18 @@
-import { NextResponse } from "next/server"
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-
-  if (!auth().userId) {
-    //  If user tries to access a private route without being authenticated,
-    //  redirect them to the sign in page
+  if (isProtectedRoute(req)) {
     const url = new URL(req.nextUrl.origin)
-    url.pathname = "/signin"
-    return NextResponse.redirect(url)
+
+    const unauthenticatedUrl = `${url.origin}/signin`
+    const unauthorizedUrl = `${url.origin}/dashboard/stores`
+
+    auth().protect({
+      unauthenticatedUrl,
+      unauthorizedUrl,
+    })
   }
 })
 
