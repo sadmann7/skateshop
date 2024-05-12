4be53b60ce21e5d5d783879249f70105de4e85f1
sadmann7 <sadmann898@gmail.com>
2024-05-12 12:33:12 +0600

    chore: update env.example

--- src/env.js
+++ src/env.js
@@ -16,6 +16,8 @@ export const env = createEnv({
     EMAIL_FROM_ADDRESS: z.string().email(),
     UPLOADTHING_SECRET: z.string().min(1),
     UPLOADTHING_APP_ID: z.string().min(1),
+    UPSTASH_REDIS_REST_URL: z.string().url(),
+    UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
     STRIPE_API_KEY: z.string().min(1),
     STRIPE_WEBHOOK_SECRET: z.string().min(1),
     STRIPE_STD_MONTHLY_PRICE_ID: z.string().min(1),
@@ -52,6 +54,8 @@ export const env = createEnv({
     EMAIL_FROM_ADDRESS: process.env.EMAIL_FROM_ADDRESS,
     UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
     UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
+    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     STRIPE_API_KEY: process.env.STRIPE_API_KEY,
